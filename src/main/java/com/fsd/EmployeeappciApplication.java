f461a991f7fb6074106d4b7dcef2c60a530fc965
karrasankar158 <karrasankar09@gmail.com>
2024-11-20 16:08:57 +0530

    github actions continues integration

---
+++ src/main/java/com/fsd/EmployeeappciApplication.java
@@ -0,0 +1,13 @@
+package com.fsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeappciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeappciApplication.class, args);
+	}
+
+}
